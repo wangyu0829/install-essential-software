69f61c41af653116e380a9048238f7fb7e8b6dc0
WangYu <wangyu082990@hotmail.com>
2024-05-18 04:15:53 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -79,27 +79,27 @@ source /root/.zshrc;
 
 echo -e "${GREEN}nrm 安装完成${NC}"
 
-# 5. 提示输入要下载的代码库连接地址
-# 提示用户是否需要克隆远程代码库
-read -p "是否需要克隆远程代码库？（输入“是”或“否”）:" user_choice
+# # 5. 提示输入要下载的代码库连接地址
+# # 提示用户是否需要克隆远程代码库
+# read -p "是否需要克隆远程代码库？（输入“是”或“否”）:" user_choice
 
 
-if [[ "$user_choice" == "是" ]]; then
-    # 提示用户输入远程地址
-    read -p "请输入远程地址（类似于 https://xxx.xxx.xxx.xxx/git/bc.txt）: " remote_url
+# if [[ "$user_choice" == "是" ]]; then
+#     # 提示用户输入远程地址
+#     read -p "请输入远程地址（类似于 https://xxx.xxx.xxx.xxx/git/bc.txt）: " remote_url
 
-    # 使用换行符分隔仓库地址
-    IFS=$'\n' read -ra repo_array <<< "$(curl -s "$remote_url")"
+#     # 使用换行符分隔仓库地址
+#     IFS=$'\n' read -ra repo_array <<< "$(curl -s "$remote_url")"
 
-    # 循环克隆仓库
-    for repo_url in "${repo_array[@]}"; do
-        git clone "$repo_url"
-    done;
+#     # 循环克隆仓库
+#     for repo_url in "${repo_array[@]}"; do
+#         git clone "$repo_url"
+#     done;
 
-    echo -e "${GREEN}已完成逐个克隆到本地。 ${NC}"
-else
-    echo -e "${YELLOW}用户选择不克隆远程代码库。${NC}"
-fi
+#     echo -e "${GREEN}已完成逐个克隆到本地。 ${NC}"
+# else
+#     echo -e "${YELLOW}用户选择不克隆远程代码库。${NC}"
+# fi
 
 
 if [[ !($(ls -a /root | grep oh-my-zsh)) ]]; then
