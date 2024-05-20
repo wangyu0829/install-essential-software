879834ee5dec2ca09e0fc7c292cb0347c3ae7d9a
WangYu <wangyu082990@hotmail.com>
2024-05-21 02:59:30 +0800

    Update clonecode.sh

--- clonecode.sh
+++ clonecode.sh
@@ -5,16 +5,17 @@ GREEN='\033[0;32m' # 绿色
 RED='\033[0;31m'   # 红色
 YELLOW='\033[1;33m' # 黄色
 NC='\033[0m'       # 恢复默认颜色
-# 提示用户是否需要克隆远程代码库
-read -p "是否需要克隆远程代码库？（输入“是”或“否”）: " response
 
-if [[ $response -eq "是" ]]; then
+# 提示用户是否需要克隆远程代码库
+read -p "是否需要克隆远程代码库？（输入“是”或“否”/ Y or n）: " response
+
+if [[ $response == "是" || $response == 'Y' || $response == 'y' ]]; then
     # Prompt user to input file containing list of repository URLs
     read -p "请输入远程地址（类似于 https://xxx.xxx.xxx.xxx/git/bc.txt: " url_file
 
     # Check if file exists
     if [ ! -f "$url_file" ]; then
-        echo "Error: 文件不存在 '$url_file'."
+        echo -e "${RED}Error: 文件不存在 '$url_file'.${NC}"
         exit 1
     fi
 
@@ -26,5 +27,5 @@ if [[ $response -eq "是" ]]; then
 
     echo -e "${GREEN}已完成逐个克隆到本地。 ${NC}"
 else
-    echo -e "${GREEN}已完成逐个克隆到本地。 ${NC}"
+    echo -e "${GREEN}不需要克隆代码，脚本执行完成。 ${NC}"
 fi
